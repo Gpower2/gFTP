121bd40e5ff60422197ce813658ebd60e02fd0f5
Gpower2 <arslanoglou.georgios@gmail.com>
2020-02-18 19:43:44 +0200

    Wait for the ftp refresh before showing the success message box

--- gFtpGUI/frmMain.cs
+++ gFtpGUI/frmMain.cs
@@ -669,7 +669,7 @@ namespace gFtpGUI
                 }
 
                 // Refresh the ftp grid
-                btnRefreshFtpPath_Click(null, null);
+                Task.Run(() => btnRefreshFtpPath_Click(null, null)).Wait();
 
                 MessageBox.Show($"The remote file(s) was deleted successfully!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
